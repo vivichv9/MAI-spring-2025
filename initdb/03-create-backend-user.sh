c8d4206ccaedf6aeb7b675a742bb4e8e996773d9
TimeB <timairisov@gmail.com>
2025-06-01 16:24:31 +0300

    deleted trash

--- initdb/03-create-backend-user.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER backend WITH PASSWORD 'backend';
-	CREATE DATABASE backend WITH OWNER backend;
-	GRANT ALL PRIVILEGES ON DATABASE backend TO backend;
-EOSQL
